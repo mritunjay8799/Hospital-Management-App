78c3d2e2ae706cb3ae428e2fa5612e5938303419
mritu <mritu@MT_VIVOBOOK>
2026-02-24 11:48:34 +0530

    Add project files.

---
+++ Class/Patient.cs
@@ -0,0 +1,53 @@
+﻿namespace Hospital_Management_App.Class
+{
+    public class Patient
+    {
+        private readonly string? name;
+        private readonly int age;
+        private readonly string? bloodtype;
+        private readonly List<string?> medicalHistory;
+
+        public Patient(string _name, int _age, string _bloodType)
+        {
+            name = _name;
+            age = _age;
+            bloodtype = _bloodType;
+            medicalHistory = new List<string?>();
+        }
+
+        public string Name => name!;
+        public int Age => age;
+        public string BloodType => bloodtype!;
+        public IReadOnlyList<string?> MedicalHistory => medicalHistory.AsReadOnly();
+
+
+        public void AddDiagnosis(string _diagnosis)
+        {
+            if (string.IsNullOrEmpty(_diagnosis)) throw new ArgumentException("Diagnosis is not correct.");
+
+            medicalHistory.Add(_diagnosis);
+        }
+
+        public void Summary()
+        {
+            Console.WriteLine("---------Pateint Summary-------");
+            Console.WriteLine($"Name              :    {Name}");
+            Console.WriteLine($"Age               :    {Age}");
+            Console.WriteLine($"BloodType         :    {BloodType}");
+            Console.WriteLine($"Medical History");
+
+            if (medicalHistory.Count == 0)
+            {
+                Console.WriteLine("No Medical History");
+            }
+            else
+            {
+                foreach (string rec in medicalHistory)
+                {
+                    Console.WriteLine($"=> {rec}");
+                }
+            }
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Hospital_Management_App.Class;
+/*
+Build: Hospital Management Console
+
+You're the sole developer hired by City General Hospital. They manage everything on paper and want a digital system. 
+Across 5 principles, you'll build their complete console app — admitting patients, assigning staff, running daily rounds, 
+and printing reports. Each principle unlocks a new chapter of the system.
+*/
+
+
+/*
+
+ Create a `Patient` class. Keep `name`, `age`, `bloodType`, `medicalHistory` private. Expose getters(read value) for each and add 
+`addDiagnosis(entry)`. Print a formatted patient summary to the console.
+
+ */
+
+Patient patient = new Patient("Mahesh", 45, "A+");
+patient.AddDiagnosis("Malaria");
+patient.AddDiagnosis("TuberColosis");
+
+patient.Summary();
